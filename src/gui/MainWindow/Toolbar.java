5dd233d82150000940540646ff40a2866d5670c0
Andrzej Markiewicz <70719076+andrzejmarkiewicz@users.noreply.github.com>
2021-02-05 20:40:23 +0100

    Add files via upload

---
+++ src/gui/MainWindow/Toolbar.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui.MainWindow;
+
+import gui.Listeners.ExitButtonListener;
+import controller.Controller;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+/**
+ *
+ *
+ * @author andrzej
+ */
+public class Toolbar extends JPanel {
+    
+    private JButton exitButton;
+    private ExitButtonListener exitButtonListener;
+
+    public Toolbar() {
+        
+        exitButton = new JButton("Exit");
+        
+        setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        setLayout(new FlowLayout(FlowLayout.RIGHT));
+        
+        add(exitButton);
+        
+        setBackground(new Color(174,217,246));
+        
+        exitButton.setBackground(new Color(86, 157, 219));
+        exitButton.setBorderPainted(true);
+        exitButton.setFocusPainted(false);
+        exitButton.setPreferredSize(new Dimension(70, 25));
+        
+        
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                System.out.println("SAVING TO DATABASE");
+                exitButtonListener.exitButtonPressed();
+                
+            }
+        });
+    }
+
+    public void setExitButtonListener(ExitButtonListener exitButtonListener) {
+        this.exitButtonListener = exitButtonListener;
+    } 
+}
