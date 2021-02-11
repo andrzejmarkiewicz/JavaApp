810e1193127c5d15e701a15905a7b00fc7e8c386
Andrzej Markiewicz <70719076+andrzejmarkiewicz@users.noreply.github.com>
2021-02-11 19:46:42 +0100

    Update Database.java

--- src/model/Database.java
+++ src/model/Database.java
@@ -5,10 +5,6 @@
  */
 package model;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,61 +17,33 @@ import java.util.Map;
  */
 public class Database {
 
-    // this needs to be saved
-    // saved will be only the status of Lesson Object
-    // if the lesson is unlocked
-    // etc.
     private Map<String, Lesson> mapTextToLesson;
-    
-    
 
     public Database() {
-        
-//        System.out.println("START TIME");
-//        long startTime = System.currentTimeMillis();
-//        Lesson lesson1 = new Lesson("Lektion 1");
-//        Lesson lesson2 = new Lesson("Lektion 2");
-//        mapTextToLesson.put("Lektion 1", lesson1);
-//        mapTextToLesson.put("Lektion 2", lesson2);
-//        long endTime = System.currentTimeMillis();
-//        System.out.print("Difference: ");
-//        System.out.println(endTime - startTime);
-       
-
         initializeDatabase();
     }
     
     private void initializeDatabase() {
         mapTextToLesson = new HashMap<>();
         LessonNumbered[] values = LessonNumbered.values();
+        
         for(LessonNumbered lesson : values) {
             mapTextToLesson.put
             (lesson.getDescription(), new Lesson(lesson.getDescription()));
         }
         
-        // setting first lesson as is open for view.
-//        mapTextToLesson.get("Lektion 1").setOpenForView();
-        mapTextToLesson.get("Lektion 1").setUnlocked(); // TEST
-        
-        
+        mapTextToLesson.get("Lektion 1").setUnlocked();
+
     }
     
     public String getFromDatabase(String key) {
         
         if(mapTextToLesson.containsKey(key)) {
-//            System.out.println("--------------------------------");
-//            System.out.println("THE LESSON IS ALREADY IN MAP!!!!!!");
-//            System.out.println("--------------------------------");
             return mapTextToLesson.get(key).getLessonsText();
         }
         
-        
         Lesson lesson = new Lesson(key);
         mapTextToLesson.put(key, lesson);
-        
-//        System.out.println("-----------------------------");
-//        System.out.println("THIS LESSON IS NOT IN THE MAP YET!!!!!!!");
-//        System.out.println("----------------------------");
         
         return mapTextToLesson.get(key).getLessonsText();
     }
@@ -103,8 +71,6 @@ public class Database {
     public String[] getRightAnswers(String key) {
         return mapTextToLesson.get(key).getRightAnswers();
     }
-    
-    
 
     public Map<String, Boolean> getLessonsStatus() {
         Map<String, Boolean> mapStringToBoolean = new HashMap<>();
@@ -115,29 +81,6 @@ public class Database {
             mapStringToBoolean.put((key), value.isUnlocked());
         }
         return mapStringToBoolean;
-    }
-    
-
-    private String retrieveTextFromFile(String text) throws FileNotFoundException {
-        
-        File file = new File(text);
-        StringBuilder outcomingTex = new StringBuilder();
-        
-        
-        BufferedReader br = new BufferedReader(new FileReader(text));
-        try {
-            String line = br.readLine();
-
-            while (line != null) {
-                outcomingTex.append(line);
-                outcomingTex.append(System.lineSeparator());
-                line = br.readLine();
-            }
-
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-        return outcomingTex.toString();
     }
     
     public void saveToFile() {
@@ -160,11 +103,9 @@ public class Database {
             mapTextToLesson = i.readFile();
             long timeEnd = System.currentTimeMillis();
             
-        } catch (ClassNotFoundException ex) {
+        } catch (Exception e) {
 
-        } catch (IOException ex) {
-
-        }   
+        } 
     }
 
     public void setLessonStatus(String text) {
@@ -181,8 +122,6 @@ public class Database {
     }
     
     public boolean checkIfUnlocked(String text) {
-        // text -> check if previous unlocked
-
         String[] parts = text.split(" ");
 
         if(parts[1].equals("1")) return true;
